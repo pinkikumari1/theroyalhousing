095e2a8bc9b9237fed826013f0c6edd113ae998a
pinkikumari1 <pinki22@navgurukul.org>
2024-02-01 23:42:38 +0530

    upload files

---
+++ script.js
@@ -0,0 +1,11 @@
+// const scriptURL = "https://script.google.com/macros/s/AKfycbyj1p3kExVbHO553Ea76FbTA7e2wE6_k4odby_U34kJmknQ6C65bx84Q0u6E1FAJ3XGPw/exec"
+
+// const form = document.forms['google'];
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//         .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+//         .catch(error => console.error('Error!', error.message));
+// });
+
